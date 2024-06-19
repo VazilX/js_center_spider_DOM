f8e130eebd9307eb3b04944c1eb62c5c99540b8a
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-19 08:21:51 +0200

    make build

---
+++ index.0465b08e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");if(t&&e){var c=t.offsetWidth,l=t.offsetHeight,o=e.clientWidth,i=e.clientHeight;t.style.left="".concat((o-c)/2,"px"),t.style.top="".concat((i-l)/2,"px")}
+//# sourceMappingURL=index.0465b08e.js.map
